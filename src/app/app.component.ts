66b1feb1df553946800d6e115f2eadb53db82084
Kamal dev Bhuyan <kamaldev.bhuyan@gmail.com>
2021-05-27 11:22:55 +0530

    appRef added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-git-deploy';
+
+  constructor(private appRef: ApplicationRef) {}
+  
+  ngOnInit(): void {
+    console.log(this.appRef);
+  }
+
 }
